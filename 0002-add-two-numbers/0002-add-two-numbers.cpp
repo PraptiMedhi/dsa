e772c5156585255619f08c7c64edab625ab6045a
Prapti Medhi <prapti.medhi07@gmail.com>
2026-06-30 11:57:42 +0530

    Time: 8 ms (7.34%), Space: 77.2 MB (12.62%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,90 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    private:
+    ListNode* reverse(ListNode* head){
+    ListNode* prev=NULL;
+    ListNode* curr=head;
+    ListNode* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+    }
+
+void insertattail(ListNode* &head, ListNode* &tail ,int val){
+     ListNode* temp=new ListNode(val);
+     if(head==NULL){
+        head=temp;
+        tail=temp;
+     }
+     else{
+        tail->next=temp;
+        tail=temp;
+     }
+
+}
+  ListNode* add(ListNode* l1, ListNode* l2){
+    int carry=0;
+    ListNode* anshead=NULL;
+    ListNode* anstail=NULL;
+    while(l1!= NULL && l2!=NULL){
+        int sum=carry+l1->val+l2->val;
+        int digit=sum%10;
+       
+       insertattail(anshead,anstail,digit);
+       carry=sum/10;
+       l1=l1->next;
+       l2=l2->next;
+    }
+    while(l1!=NULL){
+         int sum=carry+l1->val;
+        int digit=sum%10;
+       
+       insertattail(anshead,anstail,digit);
+       carry=sum/10;
+       l1=l1->next;
+    }
+    while(l2!=NULL){
+         int sum=carry+l2->val;
+        int digit=sum%10;
+       
+       insertattail(anshead,anstail,digit);
+       carry=sum/10;
+       l2=l2->next;
+    }
+    while(carry!=0){
+        int sum=carry;
+        int digit=sum%10;
+       
+       insertattail(anshead,anstail,digit);
+       carry=sum/10;
+    }
+    return anshead;
+    
+  }
+
+    
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        // l1=reverse(l1);
+        // l2=reverse(l2);
+
+        ListNode* ans=add(l1,l2);
+
+        //  ans=reverse(ans);
+        return ans;
+    }
+};
