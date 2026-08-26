8ca9c903ab8bc8db2acb9bc9967ea8fcac81b656
Prapti Medhi <prapti.medhi07@gmail.com>
2026-08-26 12:20:35 +0530

    Time: 3 ms (63.78%), Space: 15.2 MB (14.1%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+
+        stack<string> st;
+
+        while(getline(ss, token, '/')) {
+
+            if(token == "" || token == ".") {
+                continue;
+            }
+
+            if(token == "..") {
+                if(!st.empty()) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push(token);
+            }
+        }
+
+        string ans = "";
+
+        while(!st.empty()) {
+            //isse reerse firse nhi karna padega 
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+
+        if(ans == "") {
+            return "/";
+        }
+
+        return ans;
+    }
+};
